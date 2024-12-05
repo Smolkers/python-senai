2bad8cc9f7e4d46d7f038957d9d173a7874a852d
Smolkers <samuelsferreira1@gmail.com>
2024-12-05 11:26:08 -0300

    aprendi sobre variaveis

---
+++ aula 2.py
@@ -0,0 +1,12 @@
+print('Bom dia e boa noite Senhoras e senhores')
+nome_usuario = 'Samuel de Souza' # tipo -> str = string
+idade_do_usuário = 18 # tipo -> int = integer 
+altura_do_usuário = 1.77 # tipo -> fLOAT = flutuante
+maior_idade = True # tipo -> bool = boolean
+print('Seja Bem-Vindo', nome_usuario )
+print('Seja_Bem-vindo')
+nome_usuario = 'Samuel de souza' # tipo -> str = string
+idade_do_usuário,'anos' = 18 # tipo -> int = integer
+altura_do_usuário, 'metros' = 1.77 # tipo -> float = flutuante
+'você_e_maior_de_idade', maior_idade = True # tipo -> bool = boolean
+print('Seja Bem-Vindo', nome_usuario)
